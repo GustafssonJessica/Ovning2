7276625d3b770281f1e4da68a2c60b62e8bdc5e3
GustafssonJessica <jessica.gustafsson@live.se>
2025-05-26 15:48:19 +0200

    Grund till menyval 3 (upprepa ord). plus flyttat bion till case 1 och 2

--- Ovning2/Program.cs
+++ Ovning2/Program.cs
@@ -1,66 +1,75 @@
 ﻿
 bool loop = true;
 
-while (loop)
+do
 {
     Console.WriteLine("Du har kommit till huvudmenyn.\nFör att navigera och testa nya funktioner, skriv in siffra på den funktion du vill prova:" +
-        "\n1) Se ditt pris på bio");
+        "\n0) Avsluta programmet" +
+        "\n1) Se ditt pris på bio" +
+        "\n2) Se ditt sällskaps pris på bio" +
+        "\n3) Få en text upprepad 10 gånger" +
+        "\n4) nått med tre ord ");
     string input = Console.ReadLine();
 
-    switch (input)
+    switch (input) //tror det är ok med do while, för han säger "evighetsloop" i föreläsningen om en do while
     {
         case "0":
             loop = false; // "stänger ned programmet"?
+            Environment.Exit(0); // Avslutar programmet
             break;
-        case "1": //Bio
-            Console.WriteLine("Välj alternativ: " +
-                "\n1) Jag ska gå själv på bio " +
-                "\n2)Det är fler än en person");
-            string choice = Console.ReadLine();
-
-            if (choice== "1")
+        case "1":
+            Console.WriteLine("Var god ange ålder i siffor: ");
+            int age = int.Parse(Console.ReadLine());
+            string price;
+            if (age < 20)
             {
-                Console.WriteLine("Var god ange ålder i siffor: ");
-                int age = int.Parse(Console.ReadLine());
-                string price;
+                price = "Ungdomspris: 80kr";
+            }
+            else if (age > 64)
+            {
+                price = "Pensionärspris: 90kr";
+            }
+            else
+            {
+                price = "Standardpris: 120kr";
+            }
+            Console.WriteLine(price);
+            break;
+
+
+        case "2":
+            Console.WriteLine("Hur många personer ska gå på bion?");
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int totalPrice = 0;
+            for (int i = 1; i <= numberOfPeople; i++)
+            {
+                Console.WriteLine($"Hur gammal är person nummer {i}?: ");
+                age = int.Parse(Console.ReadLine()); //använder samma age som ovan. ok?
                 if (age < 20)
-                {
-                    price = "Ungdomspris: 80kr";
-                }
+                    totalPrice += 80;
                 else if (age > 64)
-                {
-                    price = "Pensionärspris: 90kr";
-                }
+                    totalPrice += 90;
                 else
-                {
-                    price = "Standardpris: 120kr";
-                }
-                Console.WriteLine(price);
+                    totalPrice += 120;
+            }
+            Console.WriteLine($"För ett sällskap på {numberOfPeople} personer så blir kostnaden {totalPrice} kronor!");
+            break;
 
-            }
-            else if (choice == "2")
+        case "3":
+            //upprepa 10 gånger
+            Console.WriteLine("Ange en godtycklig text: ");
+            string text = Console.ReadLine();
+            Console.WriteLine("\n");
+            for (int i = 1; i <= 10; i++)
             {
-                Console.WriteLine("Hur många personer ska gå på bion?");
-                int numberOfPeople = int.Parse(Console.ReadLine());
-                int totalPrice = 0;
-                for (int i = 1; i <= numberOfPeople; i++)
-                {
-                    Console.WriteLine($"Hur gammal är person nummer {i}?: ");
-                    int age = int.Parse(Console.ReadLine());
-                    if (age < 20)
-                        totalPrice += 80;
-                    else if (age > 64)
-                        totalPrice += 90;
-                    else
-                        totalPrice += 120;
-                }
-                Console.WriteLine($"För ett sällskap på {numberOfPeople} personer så blir kostnaden {totalPrice} kronor!");
+                Console.Write($"{i}. {text},"); //nu kommer det vara komma även efter sista gången
             }
+
             break;
         default:
             Console.WriteLine("Du har angett felaktig input");
             break;
     }
-}
+} while (loop);
 
 
