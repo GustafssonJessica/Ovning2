7c5e872211f056608cd85c86620240e00bd8cbc6
GustafssonJessica <jessica.gustafsson@live.se>
2025-05-26 10:26:01 +0200

    TIllagd switch och while-loop

--- Ovning2/Program.cs
+++ Ovning2/Program.cs
@@ -1,3 +1,19 @@
 ﻿
 
-Console.WriteLine("Du har kommit till huvudmenyn. För att navigera ovh testa nya funktioner, skriv in siffror!");
+Console.WriteLine("Du har kommit till huvudmenyn.\nFör att navigera och testa nya funktioner, skriv in siffra: ");
+string input = Console.ReadLine();
+
+bool loop = true;
+
+while (loop)
+{
+    switch (input)
+    {
+        case "0":
+            loop = false;
+            break;
+        default:
+            Console.WriteLine("Du har angett felaktig input");
+            break;
+    }
+}
